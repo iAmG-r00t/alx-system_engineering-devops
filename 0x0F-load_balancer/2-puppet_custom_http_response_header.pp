bed1a271124d0fad982f3879f8ffb803a2f6bd3c
iAmG-r00t <numimickey2@gmail.com>
2022-01-12 19:51:50 +0300

    last task... 🐙😀😭👌

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,30 @@
+# Setup New Ubuntu server with nginx
+# and add a custom HTTP header
+
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+	ensure => 'installed',
+	require => Exec['update system']
+}
+
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
+}
+
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://th3-gr00t.tk/ permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+exec {'HTTP header':
+	command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
+}
--- 0x0F-load_balancer/README.md
+++ 0x0F-load_balancer/README.md
@@ -68,3 +68,8 @@
 <summary><a href="./1-install_load_balancer">1. Install your load balancer</a></summary><br>
 <a href='https://postimages.org/' target='_blank'><img src='https://i.postimg.cc/4dBWZ476/image.png' border='0' alt='image'/></a>
 </details>
+
+<details>
+<summary><a href="./2-puppet_custom_http_response_header.pp">2. Add a custom HTTP header with Puppet</a></summary><br>
+<a href='https://postimages.org/' target='_blank'><img src='https://i.postimg.cc/sD2G5kZY/image.png' border='0' alt='image'/></a>
+</details>
